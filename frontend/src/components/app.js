16f8312c4bda7604d41349a67756ed8e3ace219f
GeniusSniper <tiwei61342@gmail.com>
2021-07-20 08:39:54 -0500

    setup in app.js

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import { Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
 
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
+import ExercisesContainer from './exercises/exercises_container';
+import ExerciseContainer from './exercises/exercise_container';
 
 const App = () => (
   <div>
@@ -14,6 +16,8 @@ const App = () => (
       <AuthRoute exact path="/" component={MainPage} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
+      <Route exact path='/exercises' component={ExercisesContainer}/>
+      <Route exact path='/exercises/:exerciseId' component={ExerciseContainer}/>
     </Switch>
   </div>
 );

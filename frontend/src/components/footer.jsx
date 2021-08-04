0475230bb1cbcdc4c8fb2083a59c3565f030a634
GeniusSniper <tiwei61342@gmail.com>
2021-08-04 12:59:04 -0500

    adding css for footer icons

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,32 +1,37 @@
+import { FaGithub, FaLinkedin, FaAngellist } from "react-icons/fa";
 import React from 'react';
+import '../styles/footer.css';
 
 export default () => {
     return (
-        <div className='contact'>
-            <div className='Tyler'>
-                <div>
-                    <a href="https://angel.co/u/tyler-bernstein-5" target='_blank' rel="noopener noreferrer">AngelList</a>
+        <div>
+            <h1 className='about'>About us</h1>
+            <div className='contact'>
+                <div className='contact'>
+                    <div className='icon'>
+                        <a href="https://angel.co/u/tyler-bernstein-5" target='_blank' rel="noopener noreferrer"><FaAngellist size={32}/></a>
+                    </div>
+                    <div className='icon'>
+                        <a href="https://www.linkedin.com/in/tyler-bernstein-356ba0106/" target='_blank' rel="noopener noreferrer"><FaLinkedin size={32}/></a>
+                    </div>
+                    <div className='icon'>
+                        <a href="https://github.com/tsbernstein" target="_blank" rel="noopener noreferrer"><FaGithub size={32}/></a>
+                    </div>
                 </div>
-                <div>
-                    <a href="https://www.linkedin.com/in/tyler-bernstein-356ba0106/" target='_blank' rel="noopener noreferrer">LinkIn</a>
+                <div className='contact'>
+                    <div className='icon'>
+                        <a href="https://angel.co/u/ti-wei" target='_blank' rel="noopener noreferrer"><FaAngellist size={32}/></a>
+                    </div>
+                    <div className='icon'>
+                        <a href="https://www.linkedin.com/in/ti-wei-0a20a61b8/" target='_blank' rel="noopener noreferrer"><FaLinkedin size={32}/></a>
+                    </div>
+                    <div className='icon'>
+                        <a href="https://github.com/GeniusSniper" target="_blank" rel="noopener noreferrer"><FaGithub size={32}/></a>
+                    </div>
                 </div>
-                <div>
-                    <a href="https://github.com/tsbernstein" target="_blank" rel="noopener noreferrer">GitHub</a>
+                <div className='icon'>
+                    <a href="https://github.com/GeniusSniper/DopePT" target="_blank" rel="noopener noreferrer"><FaGithub size={32}/></a>
                 </div>
-            </div>
-            <div className='Ti'>
-                <div>
-                    <a href="https://angel.co/u/ti-wei" target='_blank' rel="noopener noreferrer">AngelList</a>
-                </div>
-                <div>
-                    <a href="https://www.linkedin.com/in/ti-wei-0a20a61b8/" target='_blank' rel="noopener noreferrer">LinkIn</a>
-                </div>
-                <div>
-                    <a href="https://github.com/GeniusSniper" target="_blank" rel="noopener noreferrer">GitHub</a>
-                </div>
-            </div>
-            <div className='sourceCode'>
-                <a href="https://github.com/GeniusSniper/DopePT" target="_blank" rel="noopener noreferrer">Some Link</a>
             </div>
         </div>
     )

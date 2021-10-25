abc2b2e7175bc07f7559a1b63c4deab2fafc84e0
GeniusSniper <tiwei61342@gmail.com>
2021-10-25 09:34:31 -0500

    setup for upload img function

--- frontend/src/components/exercises/ExerciseForm.jsx
+++ frontend/src/components/exercises/ExerciseForm.jsx
@@ -47,6 +47,7 @@ class ExerciseForm extends React.Component {
 
     handleSubmit(e){
         e.preventDefault();
+
         this.props.createExercise(this.userId, this.state)
         .then(() => {
             this.setState({
@@ -213,3 +214,56 @@ class ExerciseForm extends React.Component {
 }
 
 export default ExerciseForm;
+
+const uploadImg = async (image, croppedArea) => {
+    
+    const canvas = await getCroppedImg(image, croppedArea);
+    const canvasDataUrl = canvas.toDataURL("image/jpeg");
+    const convertedUrlToFile = dataURLtoFile(
+        canvasDataUrl,
+        "cropped-image.jpeg"
+    );
+}
+
+const getCroppedImg = (imageSrc, pixelCrop, rotation = 0) => {
+	const image = await createImage(imageSrc);
+	const canvas = document.createElement("canvas");
+	const ctx = canvas.getContext("2d");
+
+	const maxSize = Math.max(image.width, image.height);
+	const safeArea = 2 * ((maxSize / 2) * Math.sqrt(2));
+
+	// set each dimensions to double largest dimension to allow for a safe area for the
+	// image to rotate in without being clipped by canvas context
+	canvas.width = safeArea;
+	canvas.height = safeArea;
+
+	// translate canvas context to a central location on image to allow rotating around the center.
+	ctx.translate(safeArea / 2, safeArea / 2);
+	ctx.rotate(getRadianAngle(rotation));
+	ctx.translate(-safeArea / 2, -safeArea / 2);
+
+	// draw rotated image and store data.
+	ctx.drawImage(
+		image,
+		safeArea / 2 - image.width * 0.5,
+		safeArea / 2 - image.height * 0.5
+	);
+
+	const data = ctx.getImageData(0, 0, safeArea, safeArea);
+
+	// set canvas width to final desired crop size - this will clear existing context
+	canvas.width = pixelCrop.width;
+	canvas.height = pixelCrop.height;
+
+	// paste generated rotate image with correct offsets for x,y crop values.
+	ctx.putImageData(
+		data,
+		0 - safeArea / 2 + image.width * 0.5 - pixelCrop.x,
+		0 - safeArea / 2 + image.height * 0.5 - pixelCrop.y
+	);
+
+	// As Base64 string
+	// return canvas.toDataURL("image/jpeg");
+	return canvas;
+}

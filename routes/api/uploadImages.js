0d31dda1797cac9638ea6b707f0d75e0d392d891
GeniusSniper <tiwei61342@gmail.com>
2021-10-27 11:44:07 -0500

    setup upload function

--- routes/api/uploadImages.js
+++ routes/api/uploadImages.js
@@ -15,7 +15,21 @@ const s3 = new aws.S3({
 
 router.get("/test", (req, res) => res.json({ msg: "This is the uploadimages route" }));
 
+const upload = bucketName => multer({
+    storage: multers3({
+        s3,
+        bucket: bucketName,
+        metadata: (req, file, cb) =>{
+            cb(null, { fieldName: file.fieldname });
+        },    
+        key: (req, file, cb) =>{
+            cb(null, `image-${Date.now()}.jpeg`);
+        },
+    })
+})
+
 router.post('/exercises', (req, res, next) => {
+    const uploadSingle = upload('dope-pt-image-upload').single('croppedImage');
 
     return res.status(200).json({ data: req.files});
 })

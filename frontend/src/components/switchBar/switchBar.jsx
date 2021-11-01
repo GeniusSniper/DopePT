579e8cf18bac3af1db8df15d32f1bbe8fff9958b
GeniusSniper <tiwei61342@gmail.com>
2021-11-01 09:33:20 -0500

    setup container for user profile

--- frontend/src/components/switchBar/switchBar.jsx
+++ frontend/src/components/switchBar/switchBar.jsx
@@ -5,6 +5,7 @@ import ExercisesContainer from '../exercises/exercises_container';
 import ReactCalendar from '../calendar/calendar';
 import PatientsContainer from '../patients/patients_container'
 import CliniciansContainer from '../clinicians/clinician_container';
+import UserProfileContainer from '../userProfile/userProfileContainer';
 import 'react-calendar/dist/Calendar.css';
 
 class SwitchBar extends React.Component {
@@ -21,6 +22,8 @@ class SwitchBar extends React.Component {
         switch (this.state.sidebar) {
             case 'exercises':
                 return <ExercisesContainer userType={this.props.userType}/>
+            case 'userProfile': 
+                return <UserProfileContainer />
             case 'patients':
                 return <PatientsContainer/>
             case 'clinician':
@@ -58,6 +61,12 @@ class SwitchBar extends React.Component {
                             sidebar: 'exercises'
                         })}>
                         Exercises
+                    </div>
+                    <div
+                        onClick={() => this.setState({
+                            sidebar: 'userProfile'
+                        })}>
+                        User Profile
                     </div>
                     <div 
                         onClick={this.switchTabs}>
---
+++ frontend/src/components/userProfile/userProfileContainer.js
@@ -0,0 +1,14 @@
+import { connect } from 'react-redux'
+import UserProfile from './userprofile';
+
+const mSTP = state => {
+    return {
+        
+    }
+}
+
+const mDTP = dispatch => ({
+
+})
+
+export default connect(mSTP, mDTP)(UserProfile);

d638938003c79a609dec4be5155a681dc65f42a0
GeniusSniper <tiwei61342@gmail.com>
2021-11-01 19:30:35 -0500

    setting user profile page

--- frontend/src/components/userProfile/userprofile.jsx
+++ frontend/src/components/userProfile/userprofile.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import multiavatar from '@multiavatar/multiavatar';
 
 
 class UserProfile extends React.Component {
     render(){
-        return null;
+        let avatar = () => {
+            let avatarClass = document.querySelector('.avatar');
+            avatarClass.innerHTML = multiavatar('  '); 
+        }
+        return (
+            <div>
+                <div className='avatar'>{avatar()}</div>
+            </div>
+        )
     }
 }
 

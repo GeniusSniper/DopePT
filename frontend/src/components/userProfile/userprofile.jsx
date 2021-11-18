68a2262d2105f19cf1bbe8cc10aeffd437c8a665
GeniusSniper <tiwei61342@gmail.com>
2021-11-18 14:16:11 -0600

    adding handle to the userprofile page

--- frontend/src/components/userProfile/userprofile.jsx
+++ frontend/src/components/userProfile/userprofile.jsx
@@ -1,31 +1,37 @@
-import React from 'react';
+import React from "react";
 // import multiavatar from '@multiavatar/multiavatar';
-import { createAvatar } from '@dicebear/avatars';
-import * as style from '@dicebear/adventurer';
-
+import { createAvatar } from "@dicebear/avatars";
+import * as style from "@dicebear/adventurer";
 
 class UserProfile extends React.Component {
-    constructor(props){
-        super(props);
-        console.log(props)
-        this.state = {
-            avatar: createAvatar(style, {
-                seed: props.user.handle
-            }),
-            email: props.user.email,
-            phone: props.user.phone
-        }
-    }
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state = {
+      avatar: createAvatar(style, {
+        seed: props.user.handle,
+      }),
+      handle: props.user.handle,
+      email: props.user.email,
+      phone: props.user.phone,
+    };
+  }
 
-    render(){
-        return (
-            <div>
-                <div className='userProfileAvatar' dangerouslySetInnerHTML={{__html: this.state.avatar}}></div>
-                <div>Email: {this.state.email}</div>
-                <div>Phone: {this.state.phone}</div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div
+          className="userProfileAvatar"
+          dangerouslySetInnerHTML={{ __html: this.state.avatar }}
+        ></div>
+        <div>
+          <div>Handle: {this.state.handle}</div>
+          <div>Email: {this.state.email}</div>
+          <div>Phone: {this.state.phone}</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default UserProfile;

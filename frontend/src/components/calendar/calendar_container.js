a26711b8e48f57d0be5e471da8c2d49f0baaca0a
GeniusSniper <tiwei61342@gmail.com>
2021-11-24 15:12:37 -0600

    setup calendar container for updating calendar

--- frontend/src/components/calendar/calendar_container.js
+++ frontend/src/components/calendar/calendar_container.js
@@ -1,14 +1,19 @@
-import { connect } from 'react-redux'
-import Calendar from './calendar'
+import { connect } from 'react-redux';
+import { updateCalendar } from '../../actions/session_actions';
+import Calendar from './calendar';
 
 const mSTP = (state) => {
     const currentUser = state.session.currentUser || state.session.user;
+    let isDoctor = state.session.isClinician || (state.session.user && state.session.user.isClinician);
+    let userType = isDoctor ? 'clinicians' : 'patients';
     return {
-        currentUser
+        userType,
+        currentUser,
     }
 }
 
 const mDTP = dispatch => ({
+    updateCalendar: (userType, userId, data) => dispatch(updateCalendar(userType, userId, data)),
 })
 
 export default connect(mSTP, mDTP)(Calendar);
